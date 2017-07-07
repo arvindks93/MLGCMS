ab6be38f4d7a549a89654e2d82bd1853330046f1
arvinds <arvinds@MLGFLLAPTOP3>
2017-07-07 15:08:59 -0400

    test

--- MiscTestsAndPOC/SortingAlgorithms.cs
+++ MiscTestsAndPOC/SortingAlgorithms.cs
@@ -135,23 +135,25 @@ namespace MiscTestsAndPOC
         public void MergeMain()
         {
 
-            Console.Write("\nProgram for sorting a numeric array using Merge Sorting");
-            Console.Write("\n\nEnter number of elements: ");
-            int max = Convert.ToInt32(Console.ReadLine());
-            int[] numbers = new int[max];
-            for (int i = 0; i < max; i++)
-            {
-                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
-                numbers[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            Console.Write("Input int array : ");
-            Console.Write("\n");
-            for (int k = 0; k < max; k++)
-            {
-                Console.Write(numbers[k] + " ");
-                Console.Write("\n");
-            }
-            Console.WriteLine("MergeSort By Recursive Method");
+            //Console.Write("\nProgram for sorting a numeric array using Merge Sorting");
+            //Console.Write("\n\nEnter number of elements: ");
+            //int max = Convert.ToInt32(Console.ReadLine());
+            //int[] numbers = new int[max];
+            //for (int i = 0; i < max; i++)
+            //{
+            //    Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.Write("Input int array : ");
+            //Console.Write("\n");
+            //for (int k = 0; k < max; k++)
+            //{
+            //    Console.Write(numbers[k] + " ");
+            //    Console.Write("\n");
+            //}
+            //Console.WriteLine("MergeSort By Recursive Method");
+            int[] numbers = { 6, 3, 8, 4, 7 };
+            int max = 5;
             SortMerge(numbers, 0, max - 1);
             for (int i = 0; i < max; i++)
                 Console.WriteLine(numbers[i]);
